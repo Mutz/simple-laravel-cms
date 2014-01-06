8d5b979241a21e8c11246d7692d303d830616bfe
elenakolevska <elena@nosnaweb.net>
2014-01-05 21:40:17 -0300

    - Changed titles of social links in footer

---
+++ app/lang/pt/messages.php
@@ -0,0 +1,7 @@
+<?php 
+
+return array(
+	'follow_us_fb' => 'Curta a nossa pagina no Facebook',
+	'follow_us_tw' => 'Siga-nos no Twitter',
+	'write_to_us' => 'Escreva para nós',
+);
--- app/views/website/_partials/footer.blade.php
+++ app/views/website/_partials/footer.blade.php
@@ -2,10 +2,9 @@
 	<div class="social-footer">
 		<div class="container">
 			<div class="col-md-6 col-md-offset-3 text-center">
-				{{HTML::iconLink('#','',array('class'=>'social-link','title'=>'Curta a nossa pagina no Facebook'),'fa-facebook')}}
-				{{HTML::iconLink('#','',array('class'=>'social-link','title'=>'Siga-nos no Twitter'),'fa-twitter')}}
-				{{HTML::iconLink('/contato','',array('class'=>'social-link','title'=>'Siga-nos no Twitter'),'fa-envelope')}}
-				{{HTML::iconLink('#','',array('class'=>'social-link','title'=>'Siga-nos no Twitter'),'fa-google-plus')}}
+				{{HTML::iconLink('#','',array('class'=>'social-link','title'=>trans('messages.follow_us_fb')),'fa-facebook')}}
+				{{HTML::iconLink('#','',array('class'=>'social-link','title'=>trans('messages.follow_us_tw')),'fa-twitter')}}
+				{{HTML::iconLink('/contato','',array('class'=>'social-link','title'=>trans('messages.write_to_us')),'fa-envelope')}}
 			</div>
 		</div>
 	</div>
